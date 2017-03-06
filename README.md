c233dba2664e76e0a1b62c9a3852bdc686a9c744
Dmitry A <mr.ads@yandex.ru>
2017-03-06 12:40:10 +0300

    create the plugin

---
+++ README.md
@@ -0,0 +1,7 @@
+# Redmine plugin for github synchronization
+
+# TODO
+  - Synchronization GH-Issues with RM-Issues (Create/Update/Delete)
+  - Synchronization comment (Create/Update/Destroy)
+  - Add comment of pull request to Redmine Issue by branch name(like RM-XXX_some_note)
+  - Set issue label on Github by redmine status
---
+++ init.rb
@@ -0,0 +1,8 @@
+Redmine::Plugin.register :redmine_github_sync do
+  name ' Redmine plugin for github synchronization'
+  author 'LLC DealerPoint'
+  description ' Redmine plugin for github synchronization'
+  version '0.0.1'
+  url 'https://github.com/dealer-point/redmine_github_sync'
+  author_url 'https://github.com/dealer-point'
+end
